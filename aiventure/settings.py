2914b44ada2a12b42f4987c89285f6153d7e83ce
takeshi tsuruda <lazywayofcat@gmail.com>
2024-05-29 13:55:40 +0900

    環境変数の設定

--- aiventure/settings.py
+++ aiventure/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'partners.apps.PartnersConfig',
+    'image_generation.apps.Image_generationConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
