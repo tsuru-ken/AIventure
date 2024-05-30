31e6c80ff692506a5a2323bc4724ee420e9154b6
takeshi tsuruda <lazywayofcat@gmail.com>
2024-05-30 11:53:04 +0900

    usersにTOP画面作成

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-
-
 from . import views
+
+from users import views as user_views
 
 app_name = 'users'
 
 
 urlpatterns = [
     
+    path ('',user_views.IndexView.as_view(), name = 'index'),
+    path('signup/', views.SignUpView.as_view(),name='signup'),
+    path('signup_success/', views.SignUpSuccessView.as_view(),name='signup_success'),
     
-    path('', views.IndexView.as_view(),name = 'index'),
+    
     
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import  CreateView,TemplateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
 
-from django.views.generic import TemplateView
 
+class SignUpView(CreateView):
+    
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    
+    success_url = reverse_lazy('users:signup_success')
+    def form_valid(self, form):
+        
+        user = form.save()
+        self.get_object = user
+        return super().form_valid(form)
+
+
+class SignUpSuccessView(TemplateView):
+    
+    template_name = 'signup_success.html'
+    
 
 class IndexView(TemplateView):
-    
     template_name = 'index.html'
+
