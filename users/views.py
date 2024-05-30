1bbb217d813fdc890aa3968eb75e9d5752f3d5b7
takeshi tsuruda <lazywayofcat@gmail.com>
2024-05-30 15:19:56 +0900

    login logoutの作成

--- users/views.py
+++ users/views.py
@@ -7,22 +7,20 @@ from django.urls import reverse_lazy
 
 
 class SignUpView(CreateView):
-    
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
-    
     success_url = reverse_lazy('users:signup_success')
+
     def form_valid(self, form):
-        
+
         user = form.save()
         self.get_object = user
         return super().form_valid(form)
 
 
 class SignUpSuccessView(TemplateView):
-    
     template_name = 'signup_success.html'
-    
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
