05e8a0f53523b6cbcdf5ea5225e5266c8a16d4a7
takeshi tsuruda <lazywayofcat@gmail.com>
2024-05-31 11:45:14 +0900

    ai_models/admin.pyに追加

--- ai_models/admin.py
+++ ai_models/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import AIModel
 
-# Register your models here.
+# デコレーターでAIModelをDjango管理サイトに登録
+@admin.register(AIModel)
+# AIModeladminクラスはadmin.ModelAdminクラスを継承
+class AIModelAdmin(admin.ModelAdmin):
+    # 管理サイトのリスト画面で表示されるフィールド
+    list_display = ('user','image_generation','created_at','updated_at')
+    # 管理サイトの検索ボックスで検索可能なフィールド　例　user__username:ユーザーのユーザー名で検索可能
+    search_fields = ('user__username','image_generation__title')
+
