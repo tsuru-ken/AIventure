e061f3b93686a873cc8948e1424f8cf44a657de1
takeshi tsuruda <lazywayofcat@gmail.com>
2024-06-04 13:17:10 +0900

    base.htmlへのhome.AiModels iamge Generation の適用

--- image_generation/views.py
+++ image_generation/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView, DetailView
-
-
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import ImageGeneration
 
-#JsonResponse:JSON形式のレスポンスを返す用
+# JsonResponse: JSON形式のレスポンスを返す用
 from django.http import JsonResponse
 
-#request:HTTPリスクエストを送信するための標準ライブラリ
+# request: HTTPリクエストを送信するための標準ライブラリ
 import requests
 
-# json:JSONデータを操作するための標準ライブラリ
+# json: JSONデータを操作するための標準ライブラリ
 import json
 
-
 class IndexView(ListView):
-
     template_name = 'index.html'
 
 class ImageGenerationView(TemplateView):
@@ -34,26 +30,29 @@ def index(request):
 # generate_image関数
 def generate_image(request):
     # POSTリクエストの確認
-    if request.method =='POST':
-        # リクエストボディをJSONとして解析して、'input_text'を所得
+    if request.method == 'POST':
+        # リクエストボディをJSONとして解析して、'input_text'を取得
         data = json.loads(request.body)
         input_text = data.get('input_text')
 
         # ここでAPI Gatewayにリクエストを送信
-        # 画像生成APIのAPIのエンドポイントURL ,ヘッダー,ペイロードを準備
+        # 画像生成APIのAPIのエンドポイントURL, ヘッダー, ペイロードを準備
         api_url = "https://tmiskg4dm3.execute-api.us-east-1.amazonaws.com/dev"
-        headers = {'Content-Type':'application/json'}
+        headers = {'Content-Type': 'application/json'}
         payload = json.dumps({'input_text': input_text})
 
         # request.postを使用してAPIリクエストを送信し、レスポンスをJSONとして解析
         response = requests.post(api_url, headers=headers, data=payload)
         response_data = response.json()
 
-        # APIリクエsつろとレスポンスの内容をサーバーのコンソールに出力
+        # APIリクエストとレスポンスの内容をサーバーのコンソールに出力
         print('API Request:', payload)
         print('Response:', response_data)
 
         # 画像生成APIから受け取ったレスポンスデータをそのままJSON形式でクライアントに返す
         return JsonResponse(response_data)
     else:
-        return render(request, 'index/index.html')
+        # GETリクエストの場合、クエリパラメータからmodel_idを取得
+        model_id = request.GET.get('model_id')
+        context = {'model_id': model_id}
+        return render(request, 'index/index.html', context)
