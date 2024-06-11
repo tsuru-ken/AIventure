bad2c615c1ca5d28ec5d73f80dc4ee0af3333f37
takeshi tsuruda <lazywayofcat@gmail.com>
2024-06-12 00:56:02 +0900

    パートナー登録機能の確認画面実装 ロゴファイル確認中

--- partners/views.py
+++ partners/views.py
@@ -15,53 +15,57 @@ class PartnerCreateView(CreateView):
     template_name = 'partner_form.html'
     success_url = reverse_lazy('partners:index')
 
-    def form_valid(self, form):
-        request = self.request
-        product_info_name = request.POST.get('product_info_name')
-        product_info_content = request.POST.get('product_info_content')
-        product_info_image = request.FILES.get('product_info_image')
-        
-        case_study_name = request.POST.get('case_study_name')
-        case_study_content = request.POST.get('case_study_content')
-        case_study_image = request.FILES.get('case_study_image')
-        
-        if 'finalize' in request.POST:
-            response = super().form_valid(form)
-            form.instance.service_content.set(form.cleaned_data['service_content'])
-            form.instance.ai_category.set(form.cleaned_data['ai_category'])
-            form.instance.cost.set(form.cleaned_data['cost'])
-            form.instance.product_info.set(form.cleaned_data['product_info'])
-            form.instance.case_study.set(form.cleaned_data['case_study'])
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid() and 'confirm' in request.POST:
+            return self.form_confirm(form)
+        elif form.is_valid() and 'finalize' in request.POST:
+            return self.form_finalize(form)
+        elif 'back' in request.POST:
+            return self.form_invalid(form)
+        else:
+            return self.form_invalid(form)
 
+    def form_confirm(self, form):
+        return render(self.request, 'partner_confirm.html', {'form': form})
+
+    def form_finalize(self, form):
+        # Save partner instance
+        self.object = form.save()
+
+        # Save related instances for product_info and case_study
+        product_info_name = self.request.POST.get('product_info_name')
+        product_info_content = self.request.POST.get('product_info_content')
+        product_info_image = self.request.FILES.get('product_info_image')
+        case_study_name = self.request.POST.get('case_study_name')
+        case_study_content = self.request.POST.get('case_study_content')
+        case_study_image = self.request.FILES.get('case_study_image')
+
+        if product_info_name and product_info_content:
             product_info = ProductInfo(
-                partner=form.instance,
+                partner=self.object,
                 name=product_info_name,
                 content=product_info_content,
                 image=product_info_image
             )
             product_info.save()
 
+        if case_study_name and case_study_content:
             case_study = CaseStudy(
-                partner=form.instance,
+                partner=self.object,
                 name=case_study_name,
                 content=case_study_content,
                 image=case_study_image
             )
             case_study.save()
 
-            return response
-        elif 'confirm' in request.POST:
-            return render(request, 'partner_confirm.html', {'form': form})
-        return super().form_valid(form)
+        return redirect(self.success_url)
 
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            if 'confirm' in request.POST:
-                return render(request, 'partner_confirm.html', {'form': form})
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+    def form_valid(self, form):
+        return self.form_confirm(form)
+
+
+
 
 
 
