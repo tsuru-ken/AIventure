b426a27eb91d51827a801537eb7c41e62cbb2445
takeshi tsuruda <lazywayofcat@gmail.com>
2024-06-18 14:21:44 +0900

    bease.htmlへのリンクのホバー設定

--- users/urls.py
+++ users/urls.py
@@ -1,27 +1,16 @@
 from django.urls import path
 from . import views
 from users import views as user_views
-
 from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
-
 urlpatterns = [
-
-    path ('',user_views.IndexView.as_view(), name = 'index'),
-    path('signup/', views.SignUpView.as_view(),name='signup'),
-    path('signup_success/', views.SignUpSuccessView.as_view(),name='signup_success'),
-
-    path('login',
-        auth_views.LoginView.as_view(template_name="login.html"),
-        name='login'
-        ),
-
-    path('logout',
-        auth_views.LogoutView.as_view(template_name="logout.html"),
-        name='logout'
-        ),
-
-
+    path('', user_views.IndexView.as_view(), name='index'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('signup_success/', views.SignUpSuccessView.as_view(), name='signup_success'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
+    path('guest_login/<str:user_type>/', views.guest_login, name='guest_login'),
+    path('admin_view/', views.admin_view, name='admin_view'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,9 @@
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import  CreateView,TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, TemplateView
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import user_passes_test
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
@@ -12,16 +11,31 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('users:signup_success')
 
     def form_valid(self, form):
-
         user = form.save()
         self.get_object = user
         return super().form_valid(form)
 
-
 class SignUpSuccessView(TemplateView):
     template_name = 'signup_success.html'
 
-
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+def guest_login(request, user_type):
+    if user_type == 'admin':
+        user = authenticate(username='admin_guest', password='guestpassword')
+    else:
+        user = authenticate(username='guest', password='guestpassword')
+    if user is not None:
+        login(request, user)
+        return redirect('users:index')
+    return redirect('users:login')
+
+def admin_required(view_func):
+    decorated_view_func = user_passes_test(lambda u: u.is_superuser, login_url='users:login')(view_func)
+    return decorated_view_func
+
+@admin_required
+def admin_view(request):
+    return redirect('/admin/')
 
